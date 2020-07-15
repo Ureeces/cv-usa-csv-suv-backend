6a3d761be65b189c29ffe827efa36d53f0ccc01b
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-15 16:12:06 -0400

    Planned out the solution for 2nd stretch goal.

---
+++ get-new-cases-difference.js
@@ -0,0 +1,13 @@
+// Stretch Goal #2:
+// Write a function that takes in a 2D array and two date strings 
+// and returns the difference in NEW cases between those two days 
+// (this is the growth or decline in the rate of case growth)
+
+// Solution (Using our previous methods)
+// 1. Create three empty variables:
+// 1a. difference - hold the difference to return
+// 1b. newCases1 - hold the total new cases of date #1
+// 1c. newCases2 - hold the total new cases of date #2
+// 2. Set newCases1 equal to the newCases of date 1
+// 3. Set newCases2 equal to the newCases of date 2
+// 4. Get the difference between the two totals and return it
