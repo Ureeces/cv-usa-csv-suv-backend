96186b8242f38306d58821773b3f5ccfc074eeae
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-15 15:51:37 -0400

    Planned out solution for 1st stretch goal.

---
+++ get-total-cases-difference.js
@@ -0,0 +1,17 @@
+// Stretch Goal #1:
+// Write a function that takes in a 2D array and two date strings 
+// and returns the difference in total cases between those two days
+
+// Understanding the problem - we have 3 parameters:
+// 1. 2D array
+// 2. date #1
+// 3. date #2
+
+// Solution (Using our previous methods)
+// 1. Create three empty variables:
+// 1a. difference - hold the difference to return
+// 1b. total1 - hold the total cases of date #1
+// 1c. total2 - hold the total cases of date #2
+// 2. Set total1 equal to the totalCases of date 1
+// 3. Set total2 equal to the totalCases of date 2
+// 4. Get the difference between the two totals and return it
